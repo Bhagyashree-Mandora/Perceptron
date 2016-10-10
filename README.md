410696058402668b8f27548ccbb7dbd33a177c43
Bhagyashree <bhagyashree.mandora@aggiemail.usu.edu>
2016-10-10 10:09:47 -0600

    Calculates the weights required and plots graphs for training
    The program predicts the weights to get the equation of the line separating
    the two classes of output. It plots the graph for error as it is trained.
    It also plots graph for training of AND, OR, NAND, NOR boolean gates and
    demonstrates that this perceptron is insufficient for XOR gate

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Perceptron
 
-This is the implementation for a single neuron in a neural network which learns using training data and tests on test data where the data is divided into training and test sections using k-fold cross-validation technique.
+This is the implementation for a single two-input one-output neuron in a neural network which learns using training data and tests on test data where the data is divided into training and test sections using k-fold cross-validation technique.
+
+The two input perceptron is trained on sample data and using a graph, it is shown that as training proceeds, the error between predicted output and target output is reduced. This means that the perceptron is learning to predict output for cases satisfying the function (here, linearly separable functions).
+
+Here,
+h(x) = summation of (weights*input)
+g(h) = 1/(1 + exp**(-h))           ..it is the sigmoid function and acts as the activation function here. 
+
+Our goal is to find a (linear) function (w1)x1 + (w2)x2 + w0 such that it separates the two classes of outputs (0 or 1), given the two inputs. The constants (weights- w0, w1, w2) are so adjusted based on the training data to minimize the error.
+
+A number of epochs (iterations) are done for each of the training data instance and error is found as the difference between the predicted output and target output. The weights are adjusted based on the formula-
+
+delta_w = -1*learning_rate*(predicted_output - target_output)*derivative_of_g*input_value
+
+After the training, the final constants (weights) are used in the equation and it is used to determine if the test_input point (x1,x2) lies below the line or above it (class 0 or 1). This is compared to the target output to determine the accuracy. K-fold validation is used for partitioning data in training and test sets. The results are displayed through graphs.
+
+The training and graphs are also done for the boolean gates- AND, OR, NAND, NOR, XOR and the graphs are plotted. Since XOR is not linearly separable, it cannot be put into any of the classes defined by the line (w1)x + (w2)y + w0. As seen in the graph of Error vs. Epoch for XOR, the error is to the degree of e**-1 and varies very little with each epoch. That means, varying the weights of the line cannot reduce the error for this function and hence, this perceptron cannot be trained for XOR.
--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,25 @@
 import math
 import csv
 import random
+import matplotlib.pyplot as plt
 
-with open('../Data.csv', 'rb') as f:
+LEARNING_RATE = 0.001
+EPOCH = 1000
+ERROR_TOLERANCE = 0.0001
+FILE_NAME = '../data.csv'
+
+with open(FILE_NAME, 'rb') as f:
     reader = csv.reader(f)
-    data = list(reader)
+    data_set = list(reader)
 
 
-def h_function(w0, w1, w2, x1, x2):
-    return (w1 * x1) + (w2 * x2) + w0
+def h_function(weight0, weight1, weight2, input1, input2):
+    return weight0 + (weight1 * input1) + (weight2 * input2)
 
 
-def g_function(h):
-    return float(1) / (1 + math.exp(-h))
+def g_function(weight0, weight1, weight2, input1, input2):
+    h = h_function(weight0, weight1, weight2, input1, input2)
+    return float(1) / (1 + math.exp(-1 * h))
 
 
 def calculate_error(target_output, actual_output):
@@ -23,53 +30,176 @@ def derivative_of_g(g_result):
     return g_result * (1 - g_result)
 
 
-def delta_w(g_result, input_value, target_output, actual_output, learning_rate=0.001):
+def delta_w(g_result, input_value, target_output, learning_rate=0.001):
     derivative = derivative_of_g(g_result)
-    return learning_rate * (target_output - actual_output) * derivative * input_value
+    return -1 * learning_rate * (g_result - target_output) * derivative * input_value
 
 
-w0 = random.random()
-w1 = random.random()
-w2 = random.random()
+avg_error_plot = []
 
-print "w1 is: " + str(w1)
-print "w2 is: " + str(w2)
 
-error = 1
-iteration = 0
-while error > 0.0001 or iteration < 1000:
-    delta_w0 = 0
-    delta_w1 = 0
-    delta_w2 = 0
+def train(data):
+    w0 = random.uniform(-0.2, 0.2)
+    w1 = random.uniform(-0.2, 0.2)
+    w2 = random.uniform(-0.2, 0.2)
 
-    for row in data:
-        x1 = float(row[0])
-        x2 = float(row[1])
-        target_output = float(row[2])
+    average_error = 100
+    iteration = 0
+    while iteration < EPOCH and average_error > ERROR_TOLERANCE:
+        total_error = 0
 
-        h_result = h_function(w0, w1, w2, x1, x2)
-        g_result = g_function(h_result)
+        for row in data:
+            x1 = float(row[0])
+            x2 = float(row[1])
+            target_output = float(row[2])
 
-        if g_result <= 0.5:
-            actual_output = 0
+            g_result = g_function(w0, w1, w2, x1, x2)
+
+            error = calculate_error(target_output, g_result)
+            total_error += error
+
+            w0 += delta_w(g_result, 1, target_output, LEARNING_RATE)
+            w1 += delta_w(g_result, x1, target_output, LEARNING_RATE)
+            w2 += delta_w(g_result, x2, target_output, LEARNING_RATE)
+
+        iteration += 1
+        average_error = float(total_error) / len(data)
+        avg_error_plot.append(average_error)
+
+    print "w0 is: " + str(w0) + "  w1 is: " + str(w1) + "    w2 is: " + str(w2)
+    return {'w0': w0, 'w1': w1, 'w2': w2}
+
+
+def find_accuracy(test_data, weights):
+    match = 0
+    for test_case in test_data:
+        test_input1 = float(test_case[0])
+        test_input2 = float(test_case[1])
+        expected_output = float(test_case[2])
+
+        g_of_h = g_function(weights['w0'], weights['w1'], weights['w2'], test_input1, test_input2)
+        if g_of_h <= 0.5:
+            predicted_output = 0
         else:
-            actual_output = 1
+            predicted_output = 1
 
-        print "Actual o/p: " + str(actual_output)
-        error = calculate_error(target_output, actual_output)
-        print "Error: " + str(error)
+        if expected_output == predicted_output:
+            match += 1
+    return float(match) / len(test_data) * 100
 
-        if error != 0:
-            delta_w0 = delta_w(g_result, 1, target_output, actual_output, learning_rate=0.001)
-            delta_w1 = delta_w(g_result, x1, target_output, actual_output, learning_rate=0.001)
-            delta_w2 = delta_w(g_result, x2, target_output, actual_output, learning_rate=0.001)
-            w0 += delta_w0
-            w1 += delta_w1
-            w2 += delta_w2
 
-        print "w1 is: " + str(w1)
-        print "w2 is: " + str(w2)
-        # print delta_w1
-        # print delta_w2
+def n_fold_validation(data_set, n=10):
+    total_accuracy = 0
+    step = int(math.ceil(float(len(data_set)) / n))
+    for start in range(1, len(data_set), step):
+        test_set = []
+        training_set = []
 
-    iteration += 1
+        for index in range(1, len(data_set)):
+            if start <= index < (start + step):
+                test_set.append(data_set[index])
+            else:
+                training_set.append(data_set[index])
+        weights = train(training_set)
+        accuracy = find_accuracy(test_set, weights)
+        total_accuracy += accuracy
+        print "Accuracy = " + str(accuracy)
+    print "Average accuracy for n-folds: " + str(float(total_accuracy) / n)
+
+
+# For Output:
+
+print "\nN-fold cross validation for the data set (here, n=10)-> "
+n_fold_validation(data_set, 10)
+
+plt.plot(avg_error_plot, marker='o', linestyle='--', color='r', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for each of the n-folds')
+plt.legend()
+plt.show()
+
+
+# AND gate validation:
+
+with open('../and.csv', 'rb') as f:
+    reader = csv.reader(f)
+    or_data = list(reader)
+
+avg_error_plot = []
+
+and_weights = train(or_data)
+plt.plot(avg_error_plot, marker='s', linestyle='-', color='b', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for AND gate')
+plt.legend()
+plt.show()
+
+
+# OR gate validation:
+
+with open('../or.csv', 'rb') as f:
+    reader = csv.reader(f)
+    or_data = list(reader)
+
+avg_error_plot = []
+
+or_weights = train(or_data)
+plt.plot(avg_error_plot, marker='D', linestyle='-', color='g', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for OR gate')
+plt.legend()
+plt.show()
+
+
+# NAND gate validation:
+
+with open('../nand.csv', 'rb') as f:
+    reader = csv.reader(f)
+    nand_data = list(reader)
+
+avg_error_plot = []
+
+nand_weights = train(nand_data)
+plt.plot(avg_error_plot, marker='x', linestyle='-', color='m', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for NAND gate')
+plt.legend()
+plt.show()
+
+
+# NOR gate validation:
+
+with open('../nor.csv', 'rb') as f:
+    reader = csv.reader(f)
+    nor_data = list(reader)
+
+avg_error_plot = []
+
+nor_weights = train(nor_data)
+plt.plot(avg_error_plot, marker='h', linestyle='-', color='c', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for NOR gate')
+plt.legend()
+plt.show()
+
+
+# XOR gate validation:
+
+with open('../xor.csv', 'rb') as f:
+    reader = csv.reader(f)
+    xor_data = list(reader)
+
+avg_error_plot = []
+
+xor_weights = train(xor_data)
+plt.plot(avg_error_plot, marker='^', linestyle='-', color='y', label='Error')
+plt.xlabel('Epoch')
+plt.ylabel('Average Error')
+plt.title('Epoch Vs. Average Error for XOR gate')
+plt.legend()
+plt.show()
